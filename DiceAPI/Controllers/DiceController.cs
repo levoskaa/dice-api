f2deca268d863018a9589f62b5ca81c5b0d7d007
levoskaa <47329402+levoskaa@users.noreply.github.com>
2020-12-07 11:41:14 +0100

    Add greeting endpoint

--- DiceAPI/Controllers/DiceController.cs
+++ DiceAPI/Controllers/DiceController.cs
@@ -1,24 +1,31 @@
 ﻿using DiceAPI.Services;
 using DiceAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace DiceAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DiceController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DiceController : ControllerBase
+  {
+    private readonly IDiceService diceService;
+
+    public DiceController(IDiceService diceService)
     {
-        private readonly IDiceService diceService;
-
-        public DiceController(IDiceService diceService)
-        {
-            this.diceService = diceService;
-        }
-
-        [HttpGet("roll")]
-        public DiceThrowVM GetDiceRolls([FromQuery] int diceSize, [FromQuery] int diceCount = 1)
-        {
-            return diceService.Roll(diceSize, diceCount);
-        }
+      this.diceService = diceService;
     }
+
+    [HttpGet("roll")]
+    public DiceThrowVM GetDiceRolls([FromQuery] int diceSize, [FromQuery] int diceCount = 1)
+    {
+      return diceService.Roll(diceSize, diceCount);
+    }
+
+    [HttpGet("greeting")]
+    public string GreetUser([FromQuery] string name = "World")
+    {
+      return $"Hello {WebUtility.UrlDecode(name)}!";
+    }
+  }
 }
